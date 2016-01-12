601d5bd66985015d4df4c0b9f5c6defc5ac5137b
Benjamin <wangkun_mr@163.com>
2016-01-12 13:29:35 +0800

    rename

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -1,9 +1,9 @@
-/************************************************************************/
-/* 5. Longest Palindromic Substring 
-*  Given a string S, find the longest palindromic substring in S. You may 
-*  assume that the maximum length of S is 1000, and there exists one unique 
-*  longest palindromic substring.
-/************************************************************************/
+/************************************************************************
+* 5. Longest Palindromic Substring 
+* Given a string S, find the longest palindromic substring in S. You may 
+* assume that the maximum length of S is 1000, and there exists one unique 
+* longest palindromic substring.
+************************************************************************/
 #include <iostream>
 #include <string>
 using namespace std;
