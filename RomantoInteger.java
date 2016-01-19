471c59b7bf515eccd00e7b587a74027f046343e6
Benjamin <wangkun_mr@163.com>
2016-01-19 17:35:38 +0800

    13

---
+++ RomantoInteger.java
@@ -0,0 +1,55 @@
+package com.nnk.container;
+/*
+ * 13. Roman to Integer
+ * Given a roman numeral, convert it to an integer.
+ *
+ * Input is guaranteed to be within the range from 1 to 3999.
+ * */
+class Solution {
+	public int romanToInt(String s) {
+		int numeral = 0;
+		int first, second;
+		for (int i = 0; i < s.length(); ++i) {
+			first = charToInt(s.charAt(i));
+			if (i+1 < s.length()) {
+				second = charToInt(s.charAt(i+1));
+				if (first < second) {
+					first = second-first;
+					++i;
+				}
+			}
+			numeral += first;
+		}
+        return numeral;
+    }
+	private int charToInt(char ch) {
+		char c = Character.toUpperCase(ch);
+		if (c == 'I') {
+			return 1;
+		} else if (c == 'V') {
+			return 5;
+		} else if (c == 'X') {
+			return 10;
+		} else if (c == 'L') {
+			return 50;
+		} else if (c == 'C') {
+			return 100;
+		} else if (c == 'D') {
+			return 500;
+		} else if (c == 'M') {
+			return 1000;
+		}
+		return 0;
+	}
+}
+
+public class Main {
+   public static void main(String[] args) {
+	   Solution s = new Solution();
+	   System.out.println("IV: "+s.romanToInt("IV"));
+	   System.out.println("LIV: "+s.romanToInt("LIV"));
+	   System.out.println("DCXXI: "+s.romanToInt("DCXXI"));
+	   System.out.println("DLV: "+s.romanToInt("DLV"));
+	   System.out.println("MMCDXX: "+s.romanToInt("MMCDXX"));
+   }
+}
