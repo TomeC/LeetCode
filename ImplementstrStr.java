f78bfa1c1eb61000fae4863a8525534887b2384c
TomeC <wangkun_mr@163.com>
2016-02-17 22:21:28 +0800

    28

---
+++ ImplementstrStr.java
@@ -0,0 +1,18 @@
+package com.ben.Main;
+
+/*
+ * 28. Implement strStr()
+ * Implement strStr().
+ * Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ * */
+class Solution {
+    public int strStr(String haystack, String needle) {
+        return -1;
+    }
+}
+public class Main {
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.strStr("haha", "ah"));
+	}
+}
